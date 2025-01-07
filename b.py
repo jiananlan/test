fbfb611146b2cde6021fbd713a467c9ea4e9f2a0
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-07 21:38:06 +0800

    Update b.py

--- b.py
+++ b.py
@@ -1,29 +1,14 @@
-from pytube import YouTube
-from pytube.request import get, on_progress
-import requests
 
-# 修改请求头以模拟浏览器行为
-def custom_get(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    return response.text
+# 输入视频链接
+url = 'https://www.youtube.com/watch?v=4ktJ1Xbudpc'
 
-# 使用自定义的get请求替代默认的pytube.get
-YouTube._get = custom_get
+import yt_dlp
+ # 替换为你的视频链接
 
-# 输入视频URL
-video_url = 'https://www.youtube.com/watch?v=4ktJ1Xbudpc'
+ydl_opts = {
+    'outtmpl': r'a.mp4',  # 下载的视频保存的路径和文件名
+    'format': 'bestvideo+bestaudio/best',  # 下载最佳质量的视频和音频
+}
 
-# 创建YouTube对象
-yt = YouTube(video_url)
-
-# 获取最高质量的视频流
-video_stream = yt.streams.get_highest_resolution()
-
-# 下载视频
-video_stream.download()
-
-print("视频下载完成")
-
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
