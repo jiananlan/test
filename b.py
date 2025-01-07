942d5a5e532f39703e6357db481fda49e7a97a79
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-07 21:22:38 +0800

    Create b.py

---
+++ b.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+# 输入视频URL
+video_url = 'https://www.youtube.com/watch?v=4ktJ1Xbudpc'
+
+# 创建YouTube对象
+yt = YouTube(video_url)
+
+# 获取最高质量的视频流
+video_stream = yt.streams.get_highest_resolution()
+
+# 下载视频到当前目录
+video_stream.download()
+
+print("视频下载完成")
