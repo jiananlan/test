b0a4f25f98091e793e0369fae7e60f0d208e7a62
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-07 22:13:32 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,7 +1,7 @@
 import browser_cookie3
 
 # 获取 Chrome 或 Firefox 的 Cookies
-cookies = browser_cookie3.chrome()  # 或 browser_cookie3.firefox()
+cookies = browser_cookie3.firefox() # 或 browser_cookie3.firefox()
 
 # 将 Cookies 保存为 Netscape 格式的文件
 with open('cookies.txt', 'w') as f:
