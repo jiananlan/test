f496ca709404c900e369c008bbd4010021c3f549
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-07 21:28:09 +0800

    Update b.py

--- b.py
+++ b.py
@@ -2,9 +2,10 @@ from pytube import YouTube
 
 # 输入视频URL
 video_url = 'https://www.youtube.com/watch?v=4ktJ1Xbudpc'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
 # 创建YouTube对象
-yt = YouTube(video_url)
+yt = YouTube(video_url,headers=headers)
 
 # 获取最高质量的视频流
 video_stream = yt.streams.get_highest_resolution()
