ce03c12c3ee8d419f36ed217f43339759d7ca719
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-07 22:20:39 +0800

    Update c.py

--- c.py
+++ c.py
@@ -6,6 +6,6 @@ cookies = browser_cookie3.firefox() # 或 browser_cookie3.firefox()
 # 将 Cookies 保存为 Netscape 格式的文件
 with open('cookies.txt', 'w') as f:
     for cookie in cookies:
-        f.write(f".{cookie.domain}\tTRUE\t{cookie.path}\t{cookie.secure}\t{cookie.expires}\t{cookie.name}\t{cookie.value}\n")
+        f.write(f"{cookie.domain}   TRUE   {cookie.path}   {cookie.secure}   {cookie.expires}   {cookie.name}   {cookie.value}\n")
 with open('cookies.txt', 'r') as f:
     print(f.readlines())
