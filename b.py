0c1ce7eba432b1b8bec8c6dd342d22e90298f7c7
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-15 17:20:13 +0800

    Update b.py

--- b.py
+++ b.py
@@ -3,8 +3,7 @@ import os
 def find_mp4_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.webm'):
-                print(os.path.join(root, file))
+            print(os.path.join(root, file))
 
 # 指定搜索的目录，当前目录使用'.'
 directory = '.'
