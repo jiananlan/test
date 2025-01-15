8173c3b9ccf2c8ca0cbdc0db73ae4f0bdf5d0af0
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-15 19:50:12 +0800

    Update d.py

--- d.py
+++ d.py
@@ -7,17 +7,15 @@ def download_video_with_cookies(url, cookies_file, output_path='downloads'):
         'outtmpl': f'{output_path}/%(title)s.%(ext)s',  # 设置下载后的文件保存路径和命名
         'cookiefile': cookies_file,  # 使用提供的cookies文件
     }
-    print("run but pause")
-    return
 
     # 使用yt-dlp下载视频
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-if __name__ == '__main__':
-    video_url = 'https://www.youtube.com/watch?v=Kfa63swYwJw'  # 替换为实际的YouTube视频链接
-    cookies_file = 'cookies.txt'  # 替换为你自己的cookies文件路径
-    download_video_with_cookies(video_url, cookies_file)
+
+video_url = 'https://www.youtube.com/watch?v=Kfa63swYwJw'  # 替换为实际的YouTube视频链接
+cookies_file = 'cookies.txt'  # 替换为你自己的cookies文件路径
+download_video_with_cookies(video_url, cookies_file)
     
 ###########################################
 ##################下载部分##################
@@ -65,3 +63,43 @@ if __name__ == '__main__':
 #-----------------------------------------#
 #-----------------------------------------#
 #-----------------------------------------#
+
+
+from biliup.plugins.bili_webup import BiliBili, Data
+import os
+
+video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm']
+
+video_files = []
+
+
+def find_video_files():
+    global video_files, video_extensions
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            _, ext = os.path.splitext(file)
+            if ext.lower() in video_extensions:
+                video_files.append(os.path.join(root, file))
+    print(video_files)
+    return
+
+
+find_video_files()
+video = Data()
+video.title = video_files[0].split('/')[-1].split('.')[0]
+print(video.title)
+video.desc = '3分钟航空-3 Minutes of Aviation   '+video.title
+video.source = video_url
+# 设置视频分区,默认为160 生活分区
+video.tid = 232
+video.set_tag(['三分钟航空', '3 Minutes of Aviation', '航空', '飞机', '飞行'])
+with BiliBili(video) as bili:
+    c = {"bili-jct": "6d6928492ee639acb3dac9e72fdf8a60",
+         'SESSDATA': '46ab82d5%2C1752491094%2C1c29b%2A12CjABitUFFvcnSAUTzSL1NfajiCv2J4xjpUtfCyzuyFRy0yHpKIiiQanAEivZqpywK7ESVmdvd1lRRXUyV25yTVpVTXUzeVZpN29NQXFKeFF2UWVDQU5uX29GOFpBWEVmTkZObDRxWHpXejEzYmpGYzlRR2I1QUcwQ0xNRDI2SmlrbWg2V2hpQ2FBIIEC'}
+    bili.login_by_cookies(cookie=c)
+    for file in video_files:
+        video_part = bili.upload_file(file)  # 上传视频
+        video.append(video_part)  # 添加已经上传的视频
+    video.cover = bili.cover_up('/cover_path').replace('http:', '')
+    ret = bili.submit()  # 提交视频
+
