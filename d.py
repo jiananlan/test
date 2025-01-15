0f9155506fb3c03c8de8f62c7482614c1df2270f
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-15 18:20:12 +0800

    Update d.py

--- d.py
+++ d.py
@@ -3,7 +3,7 @@ import yt_dlp
 def download_video_with_cookies(url, cookies_file, output_path='downloads'):
     # 配置下载选项
     ydl_opts = {
-        'format': 'best',  # 下载最佳质量的视频
+        'format': 'bestvideo+bestaudio',  # 下载最佳质量的视频
         'outtmpl': f'{output_path}/%(title)s.%(ext)s',  # 设置下载后的文件保存路径和命名
         'cookiefile': cookies_file,  # 使用提供的cookies文件
     }
@@ -13,6 +13,6 @@ def download_video_with_cookies(url, cookies_file, output_path='downloads'):
         ydl.download([url])
 
 if __name__ == '__main__':
-    video_url = 'https://www.youtube.com/watch?v=IWSN0Sc3IrY'  # 替换为实际的YouTube视频链接
+    video_url = 'https://www.youtube.com/watch?v=Kfa63swYwJw'  # 替换为实际的YouTube视频链接
     cookies_file = 'cookies.txt'  # 替换为你自己的cookies文件路径
     download_video_with_cookies(video_url, cookies_file)
