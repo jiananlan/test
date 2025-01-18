4bb2077cd7cb310fbf8b22dcd2a1dee3f81504d7
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-01-18 12:27:56 +0800

    Update text_content.txt

--- main.go
+++ main.go
@@ -127,8 +127,8 @@ func writeFile() {
 
 func main() {
 	// 数据读取和初始化操作
-	data, _ := os.ReadFile("150ns.xyz")
-	fmt.Println(150)
+	data, _ := os.ReadFile("160ns.xyz")
+	fmt.Println(160)
 	lines := strings.Split(string(data), "\n")
 	for _, line := range lines {
 		l := strings.Split(line, "  ")
