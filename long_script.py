90fba88fd9719b73e7f5d77e44aef8e31512c8fe
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-04-15 14:13:53 +0800

    Update long_script.py

--- long_script.py
+++ long_script.py
@@ -4,3 +4,8 @@ while True:
   print("Current Date and Time:", current_datetime)
   import time 
   time.sleep(5)
+  import os
+  my_secret = os.environ['TEST_SECRET']
+  
+  # 使用 secret（以下为示例，请勿打印真实 secret）
+  print("Secret loaded successfully.",my_secret)  # 删掉敏感输出
