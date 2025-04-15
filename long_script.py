4c9d76003768846639b61b980e6d3d9868fa24ff
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-04-15 14:16:21 +0800

    Update long_script.py

--- long_script.py
+++ long_script.py
@@ -8,4 +8,4 @@ while True:
   my_secret = os.environ['TEST_SECRET']
   
   # 使用 secret（以下为示例，请勿打印真实 secret）
-  print("Secret loaded successfully.",my_secret)  # 删掉敏感输出
+  print("Secret loaded successfully.",my_secret,str(my_secret),my_secret[0],my_secret=="test")  # 删掉敏感输出
