3e42064a7eb951042ee442fdbe482fe78b46ee19
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-05-08 13:03:10 +0800

    Update Physics_is_all_you_need_rather_than_FLASH_installation.py

--- Physics_is_all_you_need_rather_than_FLASH_installation.py
+++ Physics_is_all_you_need_rather_than_FLASH_installation.py
@@ -95,7 +95,7 @@ def build_mpich(src, install_dir):
     run("make install")
     run('echo "export PATH=$install_dir/bin:\$PATH" >> ~/.bashrc')
     run(". ~/.bashrc")
-    run("ls {install_dir}/bin")
+    run(f"ls {install_dir}/bin")
     os.chdir("..")
 
 def build_hdf5(src, install_dir, mpich_bin):
